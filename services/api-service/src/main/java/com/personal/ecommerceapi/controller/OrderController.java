ba24109d77b87cb7386e470eff8d2f7f8aa100b0
Velciu A Razvan-Gabriel <92295676+RazvanV12@users.noreply.github.com>
2026-01-11 15:42:37 +0200

    Update API base paths for controllers under `/api/*` endpoints

--- services/api-service/src/main/java/com/personal/ecommerceapi/controller/OrderController.java
+++ services/api-service/src/main/java/com/personal/ecommerceapi/controller/OrderController.java
@@ -14,7 +14,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("api/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
